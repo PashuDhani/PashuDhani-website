de627e91a81f669e4da68100a37e7a6fff3c0042
Rishabh Arora <rishabharora1780@gmail.com>
2025-08-16 00:15:23 +0530

    added support for gh pages.

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,3 +3,11 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
+const nextConfig = {
+    output: 'export',
+    images: { unoptimized: true },
+    basePath: '/PashuDhani-website',
+    assetPrefix: '/PashuDhani-website/'
+  };
+  export default nextConfig;
+  
